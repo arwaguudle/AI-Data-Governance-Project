5ed92152954cc16e426c3db939cc8d5eb1ef23ca
arwaguudle <arwaguudle@gmail.com>
2026-07-23 14:53:03 +0100

    testing out for 5 different requests

--- Automating-Data-Governance-main/variations_results/survey.py
+++ Automating-Data-Governance-main/variations_results/survey.py
@@ -28,7 +28,7 @@ def init_session_state():
     # Setting up session state variables; if they dont exist
     if 'survey_items' not in st.session_state:
         random.shuffle(all_items)  # randomly shuffling the purpose prompts
-        st.session_state.survey_items = all_items[:50]  #taking only 50 item per user
+        st.session_state.survey_items = all_items[:5]  #taking only 50 item per user
         st.session_state.current_index = 0
         st.session_state.results = []
         st.session_state.user_id = f"user_{random.randint(100, 999)}"
